d09c0bb28b2053bf2b15b6cf9db1fc62d01fdc30
sanal1989 <55034623+sanal1989@users.noreply.github.com>
2020-02-26 14:17:42 +0300

    unit:compele task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(typeof(matrix) == "undefined"){
+    return new Array();
+  }else if(matrix.length == 0){
+    return new Array();
+  }
+let reverse  = false;
+let result = [];
+for(let i=0;i<matrix.length;i++){
+	if(reverse==false){
+    	for(let j=0;j<matrix[i].length;j++){
+        	result.push(matrix[i][j]);
+    	}
+    }else{
+    	for(let j=matrix[i].length-1;j>=0;j--){
+        	result.push(matrix[i][j]);
+    	}
+    }
+    if(reverse==false){
+    	reverse=true;
+    }else{
+    	reverse=false;
+    }
+}
+  return result;
 }
